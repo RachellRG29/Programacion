8747421afb15e3beb5503e6d8422cfdc9c8c78b4
rachellramirez29 <RG21019.ues.edu.sv>
2024-05-06 21:05:11 -0600

    Funciona con couch db, local, lista y agregar, faltan cambios esteticos

--- app/src/main/java/com/ugb/programacion/Login.java
+++ app/src/main/java/com/ugb/programacion/Login.java
@@ -3,10 +3,14 @@ package com.ugb.programacion;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
 
@@ -16,9 +20,16 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        //cambiar color barra estado
+        cambiarColorBarraEstado(getResources().getColor(R.color.blueblue));
+
         //valores para usuario y contraseña
         EditText txtUsuarioL= (EditText)findViewById(R.id.txt_usuario_login);
         EditText txtContrasenaL= (EditText)findViewById(R.id.txt_contrasena_login);
+
+        //txt lbl_signup_aActivitySignup a Registrar en caso de no tener cuenta
+        TextView lblRegistrar = (TextView) findViewById(R.id.lbl_login_aActivitySignup);
+        lblRegistrar.setOnClickListener(v -> openRegistrarlbl());
 
         //boton ingresar
         Button btnPrincipal = (Button) findViewById(R.id.btn_ingresar);
@@ -31,7 +42,7 @@ public class Login extends AppCompatActivity {
                     return;
 
                 } else {
-                    openLista();
+                    irLista();
                 }
             }
         });
@@ -42,11 +53,32 @@ public class Login extends AppCompatActivity {
     //Private voids
 
     //abrir la lista principal
-    private void openLista(){
+    private void irLista(){
         Intent abrirVentana = new Intent(getApplicationContext(), lista_delivery.class);
         startActivity(abrirVentana);
 
     }
+
+    //abrir view main activity Registrar o Sign up
+    public void openRegistrarlbl(){
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+
+    }
+
+    //Metodo para cambiar el color de la barra de estado
+    private void cambiarColorBarraEstado(int color) {
+        // Verificar si la versión del SDK es Lollipop o superior
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(color);
+        }
+    } //fin cambiar colorbarraestado
+
+
+
+
 
 
 } //fin login
